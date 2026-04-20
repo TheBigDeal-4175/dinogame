aecece699bf29b3bd574ecef7792534e1cad4299
Abram Votrobeck <abram.votrobeck@columbiachristian.com>
2026-04-20 08:47:20 -0700

    fixing stuffs

--- Cactus.js
+++ Cactus.js
@@ -2,33 +2,12 @@ import { Sprite } from './sprite.js'
 
 export class Cactus extends Sprite {
     constructor (game) {
-        this.game = game
-        this.x = 400        // right side of floor level
         super(game)
         this.x = 200        // right side of floor level
         this.y = 400
     
         this.current_sprite = "cactus1"
     }
-    draw(ctx) {
- 
-        this.scale = 0.5
-        var current_sprite = "cactus"
-        var sprite = this.game.sprites[current_sprite]
-        // Draw Dinosaur sprite
-        ctx.drawImage(this.game.sprite_sheet,
-            sprite.x, sprite.y,
-            sprite.w, sprite.h,
-
-            // destination cornor - upper left
-            this.x - sprite.cx* this.scale,
-            this.y - sprite.cy* this.scale,
-            // destination scale
-            sprite.w * this.scale,
-            sprite.h * this.scale
-        )
-
-    } 
 
 }   
 
