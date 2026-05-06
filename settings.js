324fc81135f0cce293de77e9ed03b88886858fc8
Bryson <bryson.deal@columbiachristian.com>
2026-05-06 09:12:29 -0700

    fixed thew speed and the jump

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
     floor_y: 400,
     gravity_dy: 0.5,
-    jump_dy: 12,
+    jump_dy: 10,
     bird_height: 60,
-    bird_speed: 2.5,
-    cactus_speed: 2.5,
+    bird_speed: 1,
+    cactus_speed: 4.5,
     cactus_rate: 500,
-    cactus_frequency: 500,
+    cactus_frequency: 150,
     cactus_randomness: 100,
-    bird_frequency: 1000,
+    bird_frequency: 900,
     bird_randomness: 250,
 
 }
