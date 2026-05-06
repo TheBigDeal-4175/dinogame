ac6b29fac6d4073acb9d7571a42c48516a3d11ca
Abram Votrobeck <abram.votrobeck@columbiachristian.com>
2026-05-06 09:05:48 -0700

    asettings

--- settings.js
+++ settings.js
@@ -6,6 +6,11 @@ const settings = {
     bird_speed: 2.5,
     cactus_speed: 2.5,
     cactus_rate: 500
+    cactus_frequency: 500,
+    cactus_randomness: 100,
+    bird_frequency: 1000,
+    bird_randomness: 250,
+
 }
 
 export default settings;
